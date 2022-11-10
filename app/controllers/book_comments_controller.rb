695b32a1b6020a4ea1e40918bf65455a4ae1c217
TaroYamada <114091227+achan-m@users.noreply.github.com>
2022-11-10 09:43:45 +0000

    [Finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_params)
-    comment.book_id = book.id
-    comment.save
-    #以下2行と上の4行は同じ内容のところないのか？？？
     @book=Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_params)
+    comment.book_id = @book.id
+    comment.save
     @book_comment = BookComment.new
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    #上の文なんだ？ @book.destroyの文の意味も入ってるやつ？一文になってるのかな？→合ってました
+    @bookcomment=BookComment.find(params[:id])
+    @bookcomment.destroy
+    #BookComment.find(params[:id]).destroyが略の形
     @book=Book.find(params[:book_id])
-    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    @book_comment.destroy
     @book_comment = BookComment.new
-    #上の4行はなんだ？？？
   end
 
   private
