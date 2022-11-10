d1877d10cf91e8617781128cb003bfe9340a660a
TaroYamada <114091227+achan-m@users.noreply.github.com>
2022-11-10 05:57:32 +0000

    [Start]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,13 +4,19 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    #以下2行と上の4行は同じ内容のところないのか？？？
+    @book=Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
     #上の文なんだ？ @book.destroyの文の意味も入ってるやつ？一文になってるのかな？→合ってました
-    redirect_to book_path(params[:book_id])
+    @book=Book.find(params[:book_id])
+    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
+    #上の4行はなんだ？？？
   end
 
   private
